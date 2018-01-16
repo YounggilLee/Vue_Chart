5d052f6144be8f4ccddd039e1e552051804aac4c
GiL <iamleeyounggil@gmail.com>
2018-01-16 13:30:55 -0500

    Install Vue-wamp to get WAMP from the server and move sample chart data from .js to app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="Chart__list">
         <div class="Chart">
           <h2>Linechart</h2>
-          <line-example></line-example>
+          <line-example :data="data" :options="options"></line-example>
         </div>
       </div>
     </div>
@@ -13,8 +13,50 @@
 
 <script>
 import LineExample from './LineChart.js'
+
+var data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Data One',
+        fill: false,
+        borderColor: '#FC2525',
+        backgroundColor: '#FC2525',
+        data: [40, 39, 10, 40, 39, 80, 40]
+      },{
+        label: 'Data Two',
+        fill: false,
+        borderColor: '#05CBE1',
+        backgroundColor: '#05CBE1',
+        data: [60, 55, 32, 10, 2, 12, 53]
+      }
+    ]
+  }
+
+  var options = {responsive: true, maintainAspectRatio: false}
+
+
 export default {
   name: 'app',
+  data() {
+      return {
+        foo: 'bar',
+        data : data,
+        options: options
+
+      };
+    },
+    mounted() {
+        this.$wamp.subscribe('com.test.both', function(args, kwArgs, details) {
+            // component context is available
+           
+            return this.foo;
+        }, {
+            acknowledge: true // option needed for promise, automatically added
+        }).then(function(s) {
+            console.log('AutobahnJS Subscription object: ', s); 
+        });
+    },
   components: {
     LineExample
   }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueWamp from 'vue-wamp'
+
+Vue.use(VueWamp, {
+  debug: true,
+  url: 'ws://138.197.146.172:9000/ws',
+  realm: 'realm1',
+  onopen: function(session, details) {
+      console.log('WAMP connected', session, details);
+  },
+  onclose: function(reason, details) {
+      console.log('WAMP closed: ' + reason, details);
+  }
+})
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
